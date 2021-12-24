280ad7e19e4f536f3ff394cd623874aa1c0bcba8
yosuke ota <otameshiyo23@gmail.com>
2021-12-24 09:30:43 +0900

    Add playground

---
+++ playground/docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+/* global module, require -- global */
+"use strict"
+
+module.exports = {
+    base: "/",
+
+    head: [],
+
+    // eslint-disable-next-line node/no-unpublished-require -- ignore
+    plugins: [[require("vuepress2-plugin-full-text-search").default]],
+}
