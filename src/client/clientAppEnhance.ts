66748dd01cf98cfa107d577418f91090b25bcfad
stainge <48624558+stainge@users.noreply.github.com>
2022-05-16 00:27:10 +0800

    Update clientAppEnhance.ts

--- src/client/clientAppEnhance.ts
+++ src/client/clientAppEnhance.ts
@@ -2,9 +2,9 @@ import { defineClientConfig } from "@vuepress/client"
 import { defineAsyncComponent } from "vue"
 
 export default defineClientConfig({
-    // eslint-disable-next-line @typescript-eslint/naming-convention -- component
     enhance({ app }) {
+        // eslint-disable-next-line @typescript-eslint/naming-convention -- component
         const SearchBox = defineAsyncComponent(() => import("./SearchBox.vue"))
         app.component("SearchBox", SearchBox)
-    }
+    },
 })
