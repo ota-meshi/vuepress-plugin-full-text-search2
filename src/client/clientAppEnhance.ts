ab0680df666570fd3141ac9893aee4e42a37479b
ota-meshi <otameshiyo23@gmail.com>
2024-02-28 22:15:59 +0900

    fix

--- src/client/clientAppEnhance.ts
+++ src/client/clientAppEnhance.ts
@@ -4,13 +4,8 @@
 import { defineClientAppEnhance } from "@vuepress/client";
 import { defineAsyncComponent } from "vue";
 
-export default defineClientAppEnhance(
-  (
-    // @ts-expect-error -- Backward compatibility for vuepress@<=2.0.0-beta.43
-    { app },
-  ) => {
-    // eslint-disable-next-line @typescript-eslint/naming-convention -- component
-    const SearchBox = defineAsyncComponent(() => import("./SearchBox.vue"));
-    app.component("SearchBox", SearchBox);
-  },
-);
+export default defineClientAppEnhance(({ app }) => {
+  // eslint-disable-next-line @typescript-eslint/naming-convention -- component
+  const SearchBox = defineAsyncComponent(() => import("./SearchBox.vue"));
+  app.component("SearchBox", SearchBox);
+});
